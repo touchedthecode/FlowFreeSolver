dc92242bc5f464ec9e9d0f356ed9dfb74a991074
touchedthecode <git@touchedthecode.com>
2024-08-27 16:31:13 +0200

    Initial Commit

---
+++ FlowFreeSolver/FlowFreeSolverApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowFreeSolverApp.swift
+//  FlowFreeSolver
+//
+//  Created by Nikolaos Papandreou on 27.08.24.
+//
+
+import SwiftUI
+
+@main
+struct FlowFreeSolverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
